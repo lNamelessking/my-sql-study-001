c4385a9e9b55fa5fa8f10733920a35ac264b108f
Егор <99268649+lNamelessking@users.noreply.github.com>
2025-10-15 21:47:14 +0500

    upd: solution loaded

--- beginner/01_user_more_10_posts/solution.sql
+++ beginner/01_user_more_10_posts/solution.sql
@@ -1,1 +1,10 @@
-
+SELECT id, nickname
+FROM profile p
+JOIN (
+    SELECT 
+        owner_id, 
+        COUNT(*) as profile_posts_count
+    FROM post
+    GROUP BY owner_id
+) post_count on p.id = post_count.owner_id
+WHERE profile_posts_count > 10
